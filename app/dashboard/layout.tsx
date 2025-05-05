ea3e4eaed2157ff4806c865819f1bb97498a77c0
Rashedul Islam Maruf <mdm326040@gmail.com>
2025-05-05 10:21:47 +0600

    Use suspense in dashboard page

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import { ReactNode, Suspense } from "react";
+
+const layout = ({ children }: { children: ReactNode }) => {
+  return (
+    <Suspense
+      fallback={
+        <div className="flex h-screen w-full items-center justify-center">
+          Loading...
+        </div>
+      }
+    >
+      {children}
+    </Suspense>
+  );
+};
+
+export default layout;
